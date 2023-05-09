305225c221352441bd0134c761bbfd77a9aaae09
AmirHPartovi <a.partovi99@gmail.com>
2023-05-09 18:08:54 +0330

    theme created

---
+++ src/theme/theme.ts
@@ -0,0 +1,58 @@
+import { createTheme } from "@mui/material/styles";
+import "@fontsource/almarai";
+import "@fontsource/lato";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+          main: '#2FD1C5',
+          contrastText:'#FFF'
+        },
+        error:{
+            main:'#FF7A7B',
+        },
+        success:{
+            main:'#26BFBF',
+        },
+        grey:{
+            50:'#CCCCCC',
+            100:'#585A66',
+            200:'#2F394B',
+            300:'#00394C',
+        },
+        text:{
+            primary:'#00394C',
+            secondary:'#585A66',
+            disabled:'#CCCCCC',
+        },
+        divider:'#E4EDFF',
+        background:{
+            default:'#F5FBFF'
+        },
+    },
+    typography:{
+        fontFamily:'lato',
+        fontSize: 16,
+        fontWeightLight: 400,
+        h3:{
+            fontFamily:'almarai',
+            fontSize:'1.875rem',// 30px / 16 px
+            fontStyle: 'normal',
+            fontWeight: 700,
+            lineHeight: '120%',
+            color:'#00394C',
+        },
+        h4:{
+            fontFamily: 'Lato',
+            fontStyle: 'normal',
+            fontWeight: 700,
+            fontSize: '1.3125rem',//21px
+            lineHeight: '120%',
+            color: '#2F394B',
+
+        },
+    }
+  });
+
+
+export default theme;
